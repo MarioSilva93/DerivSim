6cd921f307e92726560f734e494e6ee61961a771
MarioSilva93 <msilvaconego@gmail.com>
2025-05-04 10:19:59 +0200

    ...

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import deliveries from "./data/deliveries";
-import { addDelivery, clearRoute, calculateRouteDistance } from "./core/routeManager";
-import DeliveryList from "./ui/DeliveryList";
-import RoutePlanner from "./ui/RoutePlanner";
-import MapView from "./ui/MapView";
-
+import deliveries from "./delivsim/src/data/deliveries";
+import { addDelivery, clearRoute, calculateRouteDistance } from "./delivsim/src/core/routeManager";
+import DeliveryList from "./delivsim/src/ui/DeliveryList";
+import RoutePlanner from "./delivsim/src/ui/RoutePlanner";
+import MapView from "./delivsim/src/ui/MapView";
+import DebugPanel from "./delivsim/src/ui/DebugPanel";
 
 export default function App() {
   const [route, setRoute] = useState([]);
@@ -13,12 +13,12 @@ export default function App() {
   const handleAdd = (delivery) => setRoute(addDelivery(route, delivery));
   const handleClear = () => setRoute(clearRoute());
 
-
   return (
     <div className="flex h-screen">
       <div className="w-1/3 p-4 bg-white overflow-y-auto">
         <DeliveryList deliveries={deliveries} onAdd={handleAdd} />
         <RoutePlanner route={route} onClear={handleClear} distance={distance} />
+        <DebugPanel route={route} distance={distance} />
       </div>
       <div className="w-2/3">
         <MapView deliveries={deliveries} route={route} />
